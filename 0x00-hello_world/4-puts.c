6c07628fcae59185ef51ff71ab15481dd5271f4c
howsolomon471 <howsolomon471@gmail.com>
2022-12-11 09:36:16 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <studio.h>
+
 /**
- * main - main block
- *return: 0
+ * main - prints a given phrase
+ * return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\"programming is like develop a multingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
 
