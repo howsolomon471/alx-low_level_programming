9633db7e23253b9be68b809b6f787634df9d0423
howsolomon471 <howsolomon471@gmail.com>
2022-12-12 04:45:58 -0800

    tsb

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- * main - Prints exactly "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - Prints"and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to standard error.
- * Return: Always 0 (Success)
+ * Return: Always 1.
  */
 
 int main(void)
