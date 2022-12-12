42619947d6afa16a5ca9b8bbb8e4363da3b5a04b
howsolomon471 <howsolomon471@gmail.com>
2022-12-12 03:58:08 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
- #include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
-* main - Prints a size of data type                                                                                                     
-*                                                                                                                                       
-* Return: Always 0 (Sucsess)                                                                                                            
-*/                                                                                                                                      
-int main(void)                                                                                                                          
-{                                                                                                                                       
-printf("Size of a char: %zu byte(s)\n", sizeof(char));                                                                                  
-printf("Size of an int: %zu byte(s)\n", sizeof(int));                                                                                   
-printf("Size of a long int: %zu byte(s)\n", sizeof(long int));                                                                          
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));                                                                
-printf("Size of a float: %zu byte(s)\n", sizeof(float));                                                                                
-return (0);                                                                                                                             
+#include <stdio.h>
+
+/**
+ * main - Prints a size of data type
+ * Return: Always 0 (Sucsess)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
