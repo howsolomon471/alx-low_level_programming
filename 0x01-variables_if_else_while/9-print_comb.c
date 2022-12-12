b00a6d8f3fe9b584e748fc798eec5f380f734b19
howsolomon471 <howsolomon471@gmail.com>
2022-12-12 08:12:39 -0800

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,18 @@
  */
 int main(void)
 {
-	       int number;
-	
-	       for (number = 0; number <= 9; number++)
-	       {
-	       putchar(number + '0');
-	       if (number < 9)
-	       {
-	       putchar(',');
-	       putchar(' ');
-	       }
-	       }
-	       putchar('\n');
-	 return (0);
-}
+	int number;
 
-   
+	for (number = 0; number <= 9; number++)
+	{
+	putchar(number + '0');
+	if (number < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
