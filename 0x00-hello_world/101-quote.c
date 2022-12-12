8a5372cad3184a1a42c56cb84c1446e7e97db2f5
howsolomon471 <howsolomon471@gmail.com>
2022-12-12 04:32:22 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - Prints exactly "and that piece of art is useful" 
  * - Dora Korpar, 2015-10-19",
  * followed by a new line, to standard error.
- * Return: Always 1.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
